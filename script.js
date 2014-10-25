f19baa33197ee5ba8c692c8f82f7244b853772b3
Ryan Wells <rawells14@gmail.com>
2014-10-24 20:34:23 -0500

    Replaced HTML files by creating a JS Navigation Bar

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 $(document).ready(function () {
-    $("#notselected").hover(function () {
-        $(this).blur
+	//Init
+	hideWrappers();
+	$(TitleWrapper).show();
+
+	//Navbar
+    $(".navbutton").click(function(event){
+    	$("#navbar li").removeClass("active");
+    	$(this).addClass("active");
+
+    	if ($("#Home").hasClass("active")){
+    		hideWrappers();
+    		$(TitleWrapper).show();
+    	};
+    	if ($("#Resume").hasClass("active")){
+    		hideWrappers();
+    		$(ResumeWrapper).show();
+    	};
+    	if ($("#Contact").hasClass("active")){
+    		hideWrappers();
+    		$(ContactWrapper).show();
+    	};
     });
-});
+
+
+    });
+
+function hideWrappers(){
+	$(TitleWrapper).hide();
+	$(ResumeWrapper).hide();
+	$(ContactWrapper).hide();
+}
 
 
 
 
-    
+
+
+
