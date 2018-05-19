0214030fd1ae589a70ff884bca0bb8f974dc7b42
Ryan <rawells14@gmail.com>
2018-05-19 16:44:50 -0500

    Cleaned out some garbage

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
 $(document ).ready(function() {
-    $("#resume-btn").click(function(){
-		$('html, body').animate({
-        scrollTop: $("#resume").offset().top
-    }, 1500);
-    });
-    $("#projects-btn").click(function(){
-		$('html, body').animate({
-        scrollTop: $("#projects").offset().top
-    }, 1500);
-    });
-    $("#about-me-btn").click(function(){
-		$('html, body').animate({
-        scrollTop: $("#about-me").offset().top
-    }, 1500);
-    });
+    // nothing functional atm
 });
